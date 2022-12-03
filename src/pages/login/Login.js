6b5d7dc643901e961eb29b1835832e187927c4cb
LuciaRyan <lcryan06@gmail.com>
2022-12-03 01:08:27 +0100

    added styling Login & changes in Home.css

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import "./Login.css"
 
 function Login({toggleLogin}) {
     return (
         <>
             <h3>Log in here </h3>
-                <div>
-            <button type="button" name="login-button" onClick={toggleLogin} >Login</button>
-                </div>
+            <div className="button-section">
+                <button type="button" className="login-button" onClick={toggleLogin}>Login</button>
+            </div>
         </>
     );
 }
