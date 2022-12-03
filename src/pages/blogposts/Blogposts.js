026d56c0f9e5afcac2f46579a57cd580997355a8
LuciaRyan <lcryan06@gmail.com>
2022-12-03 01:17:40 +0100

    added styling Blogposts.js

--- src/pages/blogposts/Blogposts.js
+++ src/pages/blogposts/Blogposts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Posts from '../../data/posts.json'
+import './Blogposts.css'
 
 import {Link} from "react-router-dom";
 
@@ -9,20 +10,18 @@ function Blogposts() {
 
     return (
         <>
-            <h3>Here is an overview of all our blogposts (private)</h3>
-            <div>
+            <div className="blogpost-title">
+                <h3>Here is an overview of all our blogposts (private)</h3>
                 <p>We currently have {numberOfPosts} blogposts on our page. Feel free to comment.</p>
             </div>
-
 
             {
                 Posts.map((post) => {
                     return (
-                        <ul>
+                        <ul className="blogpost-list">
                             <li className="post-title" key={post.id}>
                                 <Link to={`/blogpost/${post.id}`}>{post.title}</Link>
                             </li>
-
                         </ul>)
                 })
             }
